987bda532474339666a7813ee69458ea723e9324
Asanka Dharma <asankadharma@gmail.com>
2017-08-11 07:36:13 -0700

    cleaned up output from parsing

--- parse_data.py
+++ parse_data.py
@@ -4,10 +4,10 @@ import time
 from markov_python.cc_markov import MarkovChain
 from pprint import pprint
 
-
+print "reading trump tweets from JSON file"
+time.sleep (2)
 with open("trump_tweets.json", "r") as data_file:
 	data = json.load(data_file)
-print "reading JSON file+++++++++"
 
 counter = 1
 mc = MarkovChain()
@@ -15,30 +15,11 @@ for i in range(0, len(data)):
 	print (data[i]["text"])
 	mc.add_string(data[i]["text"])
 	counter += 1
-	print "***** \n"
-
 print "total number of tweets collected: %d \n" % (counter)
 
-# mc.add_file("trump_tweets.json")
-
-tweet_length = 5
+tweet_length = 200
 print "generating Markov tweets"
 print "***** \n"
 time.sleep (2)
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
-print "generating potential tweet from Trump-->" + u' '.join(mc.generate_text(tweet_length))
+for i in range(200):
+	print "Markov Trump tweet-->" + u' '.join(mc.generate_text(tweet_length))
