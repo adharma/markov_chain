7ae3ab90f3f31a2b6f80e3f4bf4b6e7d82ffa237
Asanka Dharma <asankadharma@gmail.com>
2017-08-13 08:56:05 -0700

    regex cleaning of Trump Tweets JSON file

--- parse_data.py
+++ parse_data.py
@@ -19,16 +19,27 @@ os.system('clear')
 
 """read merged JSON file trump_tweets --- into data variable"""
 print "\n****************************************"
-print "reading trump tweets from JSON file"
+print "reading trump tweets from JSON file into Markov generator"
 print "****************************************\n"
 time.sleep (sleep_time)
 with open("trump_tweets.json", "r") as data_file:
 	data = json.load(data_file)
+# with open("/etc/apt/sources.list", "r") as sources:
+#     lines = sources.readlines()
+# with open("/etc/apt/sources.list", "w") as sources:
+#     for line in lines:
+#         sources.write(re.sub(r'^# deb', 'deb', line))
 
-"""add the text of each tweet, line by line, to the Markov Chain instance 'mc'"""
+"""clean text of each tweet, then add each cleaned text line, to the Markov Chain instance 'mc'"""
 for i in range(0, len(data)):
-	print (data[i]["text"])
-	mc.add_string(data[i]["text"])
+	print "cleaning tweet--> " + (data[i]["text"])
+	string_clean1 = re.sub(r'@', '', data[i]["text"])
+	string_clean2 = re.sub(r'#', '', string_clean1)
+	string_clean3 = re.sub(r'http\S+', '', string_clean2)
+	string_clean4 = re.sub(r'pic.twitter.com\S+', '', string_clean3)
+	input_string = re.sub("(?m)^\s+", "", string_clean4)
+	print "cleaned tweet--> " + input_string
+	mc.add_string(input_string)
 	counter += 1
 time.sleep (sleep_time)
 
@@ -40,10 +51,11 @@ print "****************************************\n"
 time.sleep (sleep_time)
 
 """get start of tweet as inputs from user"""
-print "To help us generate an appropriate Trump tweet for you - enter 2 words."
-user_first_word = raw_input("Enter the first word:\n")
-user_second_word = raw_input("Enter the second word:\n")
-user_third_word = raw_input("Enter the third word of your tweet:\n")
+print "To help generate an appropriate Trump tweet for you - enter a 3 word phrase."
+print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+user_first_word = raw_input("Enter the first word:")
+user_second_word = raw_input("Enter the second word:")
+user_third_word = raw_input("Enter the third word:")
 
 """clear screen"""
 os.system('clear')
@@ -56,19 +68,25 @@ time.sleep (sleep_time)
 total_time = 0
 t1 = time.time()
 while run == True:
+	"""generate and split generated text into words"""
 	full_tweet = u' '.join(mc.generate_text())
-	print "full split array debug:" ### debugging
-	print full_tweet.split(' ') ### debugging
+	# print "full split array debug:" ### debugging
+	# print full_tweet.split(' ') ### debugging
+	""" check if full_tweet has 3 words"""
 	while len(full_tweet.split(' ')) < 3 :
 		full_tweet = u' '.join(mc.generate_text())
 	else:
 		pass
+	"""replace @ mention symbols with blanks"""
+	# for character in full_tweet:
+	# 	return character.replace("@", ""),
+	"""break full_tweet into first 3 words"""
 	first_word = full_tweet.split(' ', 1)[0]
 	second_word = full_tweet.split(' ', 2)[1]
-	print "first + second: " + first_word + " " + second_word ### debugging
-	print "full: " + full_tweet ### debugging
-
 	third_word = full_tweet.split(' ', 3)[2]
+	# print "\nDEBUG===> first + second + third: " + first_word + " " + second_word + " " + third_word ### debugging
+	# print "\nDEBUG===> full: " + full_tweet ### debugging
+	"""concatenate user inputs into a phrase"""
 	phrase = user_first_word + " " + user_second_word + " " + user_third_word
 	if re.search(phrase, full_tweet):
 		os.system('clear')
@@ -78,5 +96,4 @@ while run == True:
 		print "*----->total time to find '%s %s %s': %ds\n" % (user_first_word, user_second_word, user_third_word, total_time)
 		run = False
 	else:
-		pass
-		print "generated tweet does not match...trying again. " + first_word + " " + second_word + " " + third_word
+		print "generated text does not match...trying again. " + first_word + " " + second_word + " " + third_word
